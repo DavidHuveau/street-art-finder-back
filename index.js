8a32110497abc04fe98f5a4ee33fdbe4e1cbf3f0
David Huveau <d.huveau@gmail.com>
2019-03-05 14:14:58 +0100

    use express

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const express = require("express");
+const app = express();
+const morgan = require('morgan');
 const dotenv = require('dotenv').config();
 const mongoose = require("mongoose");
 
 const CONNECT_STRING_FULL = process.env.MONGODB_CONNECT_STRING_WITHOUT_PWD.replace(
   '<PASSWORD>',process.env.MONGODB_PASSWORD);
+const PORT = process.env.PORT;
+
+// specifies that the promises will be promises "full js" and not via bluebird
+mongoose.Promise = global.Promise;
 
 mongoose.connect(
   CONNECT_STRING_FULL,
@@ -14,13 +21,21 @@ const userSchema = new mongoose.Schema({
   age: Number
 });
 
-// spécifie que les promesses seront des promesse "pure js" et non pas via bluebird
-mongoose.Promise = global.Promise;
+const Users = new mongoose.model("User", userSchema);
 
-//pour lier une collection avec le schema
-const userModel = new mongoose.model("User", userSchema);
+app.use(morgan('dev'));
+app.get('/', (req, res) => {
+  Users
+    .find({})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => console.log(err));
+});
 
-userModel
-  .find({})
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+app.listen(PORT, err => {
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
+  console.log(`web server listening on port ${PORT}`);
+});
