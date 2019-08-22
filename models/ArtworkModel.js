f35c612b8aefae0f97e9443b1fecc16d577f0606
David Huveau <d.huveau@gmail.com>
2019-08-22 15:27:48 +0200

    change description field: min size

--- models/ArtworkModel.js
+++ models/ArtworkModel.js
@@ -1,84 +1,87 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const artworkSchema = new Schema({
-  userName: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 3
-  },
-  adressStreet: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true,
-    minlength: 3
-  },
-  zipCode: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true,
-    minlength: 3
-  },
-  city: {
-    type: String,
-    required: true,
-    uppercase: true,
-    trim: true,
-    minlength: 3
-  },
-  description: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true,
-    minlength: 5
-  },
-  photoFileName: {
-    type: String,
-    required: true
-  },
-  isActivated: {
-    type: Boolean,
-    default: true
-  },
-  isPublished: {
-    type: Boolean,
-    default: false
-  },
-  country: {
-    type: Schema.Types.ObjectId,
-    ref: 'Country',
-    required: true
-  },
-  location: {
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
+const artworkSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3
+    },
+    adressStreet: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      minlength: 3
+    },
+    zipCode: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      minlength: 3
+    },
+    city: {
+      type: String,
+      required: true,
+      uppercase: true,
+      trim: true,
+      minlength: 3
+    },
+    description: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true
+      // minlength: 5
+    },
+    photoFileName: {
+      type: String,
       required: true
     },
-    coordinates: {
-      type: [Number],
+    isActivated: {
+      type: Boolean,
+      default: true
+    },
+    isPublished: {
+      type: Boolean,
+      default: false
+    },
+    country: {
+      type: Schema.Types.ObjectId,
+      ref: "Country",
       required: true
+    },
+    location: {
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ["Point"], // 'location.type' must be 'Point'
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
     }
+  },
+  {
+    timestamps: true
   }
-}, {
-  timestamps: true
-});
+);
 
 artworkSchema.index({ location: "2dsphere" });
 
-function autoPopulateCountries (next) {
+function autoPopulateCountries(next) {
   // `this` is an instance of mongoose.Query
-  this.populate('country', 'name');
+  this.populate("country", "name");
   next();
 }
 
-artworkSchema.pre('find', autoPopulateCountries);
-artworkSchema.post('save', (doc, next) => {
-  doc.populate('country', 'name').execPopulate(() => {
+artworkSchema.pre("find", autoPopulateCountries);
+artworkSchema.post("save", (doc, next) => {
+  doc.populate("country", "name").execPopulate(() => {
     next();
   });
 });
