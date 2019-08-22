bf1d8e7b5a2dab5ab4549c65c990942b127f5fab
David Huveau <d.huveau@gmail.com>
2019-08-22 15:04:19 +0200

    add trace in console to debug

--- UploadMiddleware.js
+++ UploadMiddleware.js
@@ -1,9 +1,11 @@
 const multer = require("multer");
 
 const fileFilter = (req, file, cb) => {
+  // console.log(req.body);
+  // console.log(file);
+
   // supported image file mimetypes
   const allowedMimes = ["image/jpeg", "image/png", "image/gif"];
-
   if (!allowedMimes.includes(file.mimetype)) {
     req.fileValidationError = "goes wrong on the mimetype...";
     // return cb(new Error("goes wrong on the mimetype"));
