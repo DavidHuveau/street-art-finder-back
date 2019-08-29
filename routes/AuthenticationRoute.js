40a355199ef859af07b77369b1e6a9a191f25f7d
David Huveau <d.huveau@gmail.com>
2019-08-29 11:32:30 +0200

    enabled signup route and add expire option in the token

--- routes/AuthenticationRoute.js
+++ routes/AuthenticationRoute.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const AuthenticationController = require("../controllers/AuthenticationController");
-const bcrypt = require("bcrypt");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 const JWT_SIGN_SECRET = process.env.JWT_SIGN_SECRET;
 
-router
-  .route("/signup")
-  .get((req, res) => {
-    res.send("I am in GET signup");
-  })
-  .post(AuthenticationController.createUser);
+// router
+//   .route("/signup")
+//   .get((req, res) => {
+//     res.send("I am in GET signup");
+//   })
+//   .post(AuthenticationController.createUser);
 
 router
   .route("/signin")
@@ -22,7 +21,13 @@ router
   .post(passport.authenticate("local", { session: false }), (req, res) => {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
-    const token = jwt.sign(req.user.login, JWT_SIGN_SECRET);
+    const token = jwt.sign(
+      {
+        data: req.user.login
+      },
+      JWT_SIGN_SECRET,
+      { expiresIn: "1h" }
+    );
     return res.send({
       login: req.user.login,
       token: token,
