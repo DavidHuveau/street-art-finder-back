e4f7736a6ec4b6436412c1faba1193a43c77eab4
David Huveau <d.huveau@gmail.com>
2019-09-26 22:58:25 +0200

    remove form-data in the post

--- routes/ArtworksRoute.test.js
+++ routes/ArtworksRoute.test.js
@@ -1,7 +1,6 @@
 const supertest = require("supertest");
 const app = require("../app"); // our Node application
 const request = supertest(app);
-const FormData = require('form-data');
 
 describe("Artworks endpoints", () => {
   it("should see if Jest works", async done => {
@@ -18,41 +17,31 @@ describe("Artworks endpoints", () => {
   });
 
   it("should post a proposal that will fail", async done => {
-    const formData = new FormData();
-    formData.append("userName", "userName");
-    formData.append("adressStreet", "adressStreet");
-    formData.append("zipCode", "zipCode");
-    formData.append("city", "city");
-    formData.append("description", "description");
-    formData.append("country", "country");
-    formData.append("countryCode", "countryCode");
-
     const response = await request
       .post("/api/v1/artworks/")
-      .send({
-        formData
-      });
+      .field("userName", "userName")
+      .field("adressStreet", "adressStreet")
+      .field("zipCode", "zipCode")
+      .field("city", "city")
+      .field("description", "description")
+      .field("country", "country")
+      .field("countryCode", "countryCode");
     expect(response.status).toBe(400);
     expect(response.body).toMatchObject({message: 'Please provide an image'})
     done();
   });
 
-  xit("should post a proposal", async () => {
-    const formData = new FormData();
-    formData.append("userName", "userName");
-    formData.append("adressStreet", "adressStreet");
-    formData.append("zipCode", "zipCode");
-    formData.append("city", "city");
-    formData.append("description", "description");
-    formData.append("country", "country");
-    formData.append("countryCode", "countryCode");
-    // formData.append('my_logo', request('http://nodejs.org/images/logo.png'));
-
+  it("should post a proposal", async () => {
     const response = await request
       .post("/api/v1/artworks/")
-      .send({
-        formData
-      });
+      .field("userName", "Lisa")
+      .field("adressStreet", "26 bis boulevard Pasteur")
+      .field("zipCode", "51100")
+      .field("city", "Reims")
+      .field("description", "description")
+      .field("country", "5d891e1f6a7d8d996733f26c")
+      .field("countryCode", "FR")
+      .attach('myFile', 'samples/artworks/6caf2a4566ae219333e95053069c0eb4.png');
     console.log(response);
     expect(response.status).toBe(201);
     // expect(response.body).toMatchObject({message: 'Please provide an image'})
